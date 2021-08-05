a59e40cbc95e611238cc93e30f56bc31f7f0a1b8
lamine <lemzogueye.lg72@gmail.com>
2021-08-05 00:14:47 +0000

    adding .idea to gitigniore

--- constants.py
+++ constants.py
@@ -1,20 +1,16 @@
 SCREEN_SIZE = (1080, 608)
 
-
 NB_BULLETS = 4
 NB_LINES_ENEMIES = 4
-
 
 PLAYER_SPEED = 5
 ENEMY_SPEED = 3
 BULLET_SPEED = 7
 
-
 _SOUNDS_PATH = 'resources/sound'
 BACKGROUND_MUSIC_PATH = f'{_SOUNDS_PATH}/background_music.wav'
 GUN_SOUND_PATH = f'{_SOUNDS_PATH}/laser_gun.wav'
 GAME_OVER_MUSIC = f'{_SOUNDS_PATH}/sad_trombone.wav'
-
 
 _IMAGES_PATH = 'resources/images'
 PLAYER_IMAGE_PATH = f'{_IMAGES_PATH}/spaceship.png'
